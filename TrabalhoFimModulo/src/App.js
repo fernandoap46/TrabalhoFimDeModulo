cf42066d642da19c3e26d7eab966193f39752e49
fernandoap46 <fernandoap46@gmail.com>
2023-03-28 23:03:40 -0300

    Componentes

--- TrabalhoFimModulo/src/App.js
+++ TrabalhoFimModulo/src/App.js
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import './App.css';
-import FormUse from './Component/useState/FormUse';
+import SeuNome from './Component/State/StateLift/SeuNome';
 
 
 function App() {
+
+  const [nome, setNome] = useState()
 
   return (
 
     <div>
 
-     <FormUse/>
+     <>
+     <h1>State Lift</h1>
+     <SeuNome SeuNome={SeuNome}/>
+     
+     </>
      
     </div>
 
---
+++ TrabalhoFimModulo/src/Component/State/StateLift/SeuNome.js
@@ -0,0 +1,11 @@
+function Seunome ({setNome}){
+    return( 
+        <div>
+            <p>Digite seu nome</p>
+            <input type="text" placeholder="Qual é o seu nome" onChange={}/>
+        </div>
+
+    )
+}
+
+export default Seunome
